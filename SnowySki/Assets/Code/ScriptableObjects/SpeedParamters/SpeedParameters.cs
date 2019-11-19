d7137d2c42e8166b2236a51724d04a73edfb4273
Lisa <li.pi.97@gmx.de>
2019-11-19 21:08:42 +0100

    Move Scriptable Objects to own folder

---
+++ SnowySki/Assets/Code/ScriptableObjects/SpeedParamters/SpeedParameters.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SpeedParamters", menuName = "ScriptableObjects/SpeedParameters", order = 1)]
+public class SpeedParameters : ScriptableObject
+{
+    public float InitialSpeed = 1;
+
+    public float IncreaseEverySec = 5;
+    public float SpeedIncrement = 0.5f;
+
+    public float CurrentSpeed;
+
+    public float MaximumSpeed = 10;
+
+    void Awake()
+    {
+        Reset();
+    }
+
+    void OnEnable()
+    {
+        Reset();
+    }
+
+    void OnDestroy()
+    {
+        Reset();
+    }
+
+    public void Reset()
+    {
+        CurrentSpeed = InitialSpeed;
+    }
+}
