1c7d87bcba9f1f5e096ce150f3459d8469c99533
Lisa <li.pi.97@gmx.de>
2019-11-19 18:18:02 +0100

    Add game state manager and obstacle tag

---
+++ SnowySki/Assets/Code/Death.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    public Animator CharacterAnim;
+    public string AnimTrigSuffix;
+
+    private readonly string _hit = "isHit_";
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Obstacle"))
+        {
+            CharacterAnim.SetBool(_hit+AnimTrigSuffix, true);
+            GameStateManager.Instance.InitGameOver();
+        }
+    }
+}
---
+++ SnowySki/Assets/Code/GameStates/GameStateManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GameStateManager))]
+public class GameStateManager : SingletonBehaviour<GameStateManager>
+{
+    public Animator GameStateMachine;
+
+
+    public void InitGameOver()
+    {
+        Debug.Log("GameOver");
+    }
+
+    public void InitGameStart()
+    {
+
+    }
+}
