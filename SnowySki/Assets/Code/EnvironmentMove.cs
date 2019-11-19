ec57d3de1c0ed6376b43d6c9bc96fb2cb94177ff
Lisa <li.pi.97@gmx.de>
2019-11-19 21:09:35 +0100

    Implement environment movement and speed increase

---
+++ SnowySki/Assets/Code/EnvironmentMove.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class EnvironmentMove : MonoBehaviour
+{
+    public SpeedParameters SpeedParams;
+    public Axis MoveAxis;
+    public Transform MoveTransform;
+    public bool InvertDirection;
+
+    void Update()
+    {
+        transform.Translate( (InvertDirection ? -1 : 1) * getGlobalMovementVector() * Time.smoothDeltaTime * SpeedParams.CurrentSpeed);
+    }
+
+    Vector3 getGlobalMovementVector()
+    {
+        switch (MoveAxis)
+        {
+            case Axis.None:
+                return Vector3.zero;
+            case Axis.X:
+                return MoveTransform.right;
+            case Axis.Y:
+                return MoveTransform.up;
+            case Axis.Z:
+                return MoveTransform.forward;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
---
+++ SnowySki/Assets/Code/SpeedIncrease.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedIncrease : MonoBehaviour
+{
+    public SpeedParameters SpeedParams;
+
+    public bool IncrementSmooth = true;
+    public float SmoothIncrementTime = 0.0f;
+
+    private float _startTime = 0;
+    private float _timeSinceLastIncrease = 0;
+
+    private void OnEnable()
+    {
+        _startTime = 0;
+        _timeSinceLastIncrease = 0;
+
+        if (IncrementSmooth && SmoothIncrementTime == 0.0f)
+        {
+            SmoothIncrementTime = SpeedParams.IncreaseEverySec / 10.0f;
+        }
+    }
+
+    public void Update()
+    {
+        _timeSinceLastIncrease += Time.deltaTime;
+        if (_timeSinceLastIncrease > SpeedParams.IncreaseEverySec)
+        {
+            _timeSinceLastIncrease = 0;
+
+            if (IncrementSmooth)
+            {
+                StartCoroutine(coIncreaseSpeedSmooth(SmoothIncrementTime));
+            }
+            else
+            {
+                SpeedParams.CurrentSpeed += SpeedParams.SpeedIncrement;
+                if (SpeedParams.CurrentSpeed >= SpeedParams.MaximumSpeed)
+                {
+                    SpeedParams.CurrentSpeed = SpeedParams.MaximumSpeed;
+                }
+            }
+        }
+    }
+
+    IEnumerator coIncreaseSpeedSmooth(float smoothingTime)
+    {
+        float increase = 0;
+        float validIncrease = Mathf.Min(SpeedParams.MaximumSpeed - SpeedParams.CurrentSpeed, SpeedParams.SpeedIncrement);
+
+        while (increase < validIncrease)
+        {
+            float add = SpeedParams.SpeedIncrement / smoothingTime * Time.deltaTime;
+            increase += add;
+            SpeedParams.CurrentSpeed += add;
+            yield return null;
+        }
+    }
+}
