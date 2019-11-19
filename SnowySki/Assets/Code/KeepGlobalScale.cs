fa3943f68f2de6784c21c1dbdb1ae623ebf0a6b7
Lisa <li.pi.97@gmx.de>
2019-11-19 21:09:44 +0100

    Scale by speed

---
+++ SnowySki/Assets/Code/KeepGlobalScale.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class KeepGlobalScale : MonoBehaviour
+{
+    private Vector3 _globalScale;
+
+    void Awake()
+    {
+        _globalScale = transform.lossyScale;
+    }
+
+    void Start()
+    {
+        transform
+            .ObserveEveryValueChanged(x => x.lossyScale)
+            .DistinctUntilChanged()
+            .Subscribe(_ =>
+            {
+                transform.localScale = Vector3.one;
+
+                Vector3 adjustedScale = Vector3.one;
+                adjustedScale.Scale(_globalScale);
+                Debug.Log(divideVectors(_globalScale, transform.lossyScale));
+                adjustedScale.Scale( divideVectors(_globalScale, transform.lossyScale));
+
+                transform.localScale = adjustedScale;
+            })
+            .AddTo(this);
+    }
+
+    private Vector3 divideVectors(Vector3 nominator, Vector3 denominator)
+    {
+        return new Vector3(nominator.x/denominator.x, nominator.y/denominator.y, nominator.z / denominator.z);
+    }
+}
---
+++ SnowySki/Assets/Code/ScaleBySpeed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class ScaleBySpeed : MonoBehaviour
+{
+    public SpeedParameters SpeedParams;
+
+    public float ScaleRatio = 1;
+
+    public bool ScaleX = true;
+    public bool ScaleY = true;
+    public bool ScaleZ = true;
+
+    public void OnEnable()
+    {
+        transform.localScale = new Vector3(ScaleX ? 1 : 0, ScaleY ? 1 : 0, ScaleZ ? 1 : 0) * SpeedParams.CurrentSpeed * ScaleRatio
+                               + new Vector3(!ScaleX ? 1 : 0, !ScaleY ? 1 : 0, !ScaleZ ? 1 : 0);
+    }
+}
