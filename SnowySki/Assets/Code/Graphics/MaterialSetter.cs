98e8bb90fa7f352a0ba44320c11fb669c79cb268
Lisa <li.pi.97@gmx.de>
2019-11-23 21:33:07 +0100

    Add stuff for graphics optimization

---
+++ SnowySki/Assets/Code/Graphics/MaterialSetter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class MaterialSetter : MonoBehaviour
+{
+    public InstancedMaterialProperties InstMatProp;
+    private MeshRenderer _meshRenderer;
+    private MaterialPropertyBlock _matPropBlock;
+
+    void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _matPropBlock = new MaterialPropertyBlock();
+    }
+
+    void Update()
+    {
+        _matPropBlock.SetColor("_Color", InstMatProp.BaseColor);
+        _matPropBlock.SetVector("_TextureIndex", InstMatProp.TextureIndex);
+        _meshRenderer.SetPropertyBlock(_matPropBlock);
+    }
+}
---
+++ SnowySki/Assets/Code/ScriptableObjects/MaterialProperty/InstancedMaterialProperties.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New MaterialProperties", menuName = "ScriptableObjects/MaterialProperties", order = 1)]
+public class InstancedMaterialProperties : ScriptableObject
+{
+    public Vector2 TextureIndex;
+    public Color BaseColor;
+}
