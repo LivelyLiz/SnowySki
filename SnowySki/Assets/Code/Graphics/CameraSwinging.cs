34b67ecdbef7607fdfce1544366d91a162707c36
Lisa <li.pi.97@gmx.de>
2019-11-23 21:34:05 +0100

    Add camera swinging

---
+++ SnowySki/Assets/Code/Graphics/CameraSwinging.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class CameraSwinging : MonoBehaviour
+{
+    public float Amplitude;
+    public float Frequency;
+    public Axis MovementAxis;
+
+    private Vector3 _basePos;
+
+    void Awake()
+    {
+        _basePos = transform.position;
+    }
+
+    void Update()
+    {
+        float dirScale = Mathf.Sin(Time.time*Frequency) * Amplitude;
+        transform.position = _basePos + getMovementVector() * dirScale;
+    }
+
+    Vector3 getMovementVector()
+    {
+        switch (MovementAxis)
+        {
+            case Axis.None:
+                return Vector3.zero;
+            case Axis.X:
+                return transform.right;
+            case Axis.Y:
+                return transform.up;
+            case Axis.Z:
+                return transform.forward;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
